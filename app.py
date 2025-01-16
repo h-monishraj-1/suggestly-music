82d16e0e3729cab8121e4e1986f643a39a2f6965
h-monishraj-1 <h.monishraj.official@gmail.com>
2025-01-16 19:59:44 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify, render_template
+from music_recommendation import MusicRecommender
+import pandas as pd
+
+app = Flask(__name__)
+
+# Initialize the music recommender
+music_recommender = MusicRecommender("music_list.pkl", "similarity.pkl")
+
+# Load the music dataframe globally
+music_df = pd.read_pickle("music_list.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/recommend_music", methods=["POST"])
+def recommend_music():
+    data = request.json
+    music_title = data.get("music_title", "").strip()
+    if not music_title:
+        return jsonify({"recommendations": []})
+    
+    recommendations = music_recommender.recommend(music_title)
+    return jsonify({"recommendations": [{"title": rec[0], "poster": rec[1]} for rec in recommendations]})
+
+@app.route("/get_titles", methods=["GET"])
+def get_titles():
+    query = request.args.get("q", "").lower().strip()
+    if not query:
+        return jsonify({"titles": []})
+
+    try:
+        matching_titles = (
+            music_df[music_df['title'].str.lower().str.contains(query, case=False, na=False)]['title']
+            .head(10)
+            .tolist()
+        )
+
+        return jsonify({"titles": matching_titles})
+    except KeyError:
+        return jsonify({"titles": []})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ music_recommendation.py
@@ -0,0 +1,24 @@
+import pickle
+import requests
+import pandas as pd
+
+class MusicRecommender:
+    def __init__(self, music_path, similarity_path):
+        self.music_df = pickle.load(open(music_path, "rb"))
+        self.similarity = pickle.load(open(similarity_path, "rb"))
+
+
+    def recommend(self, music_title):
+        try:
+            music_index = self.music_df[self.music_df['title'].str.lower() == music_title.lower()].index[0]
+            distances = self.similarity[music_index]
+            music_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+            recommended_songs = []
+            for i in music_list:
+                song_title = self.music_df.iloc[i[0]]['title'].title()
+                recommended_songs.append(song_title)
+        
+            return recommended_songs
+        except IndexError:
+            return [("Song not found. Please try another.")]
